6e22d0ad460be6eed52defa924dfd56d2237e865
Yasser Esam <yaaser10esam1010@gmail.com>
2024-08-21 07:37:25 +0300

    adding card to the page but not finished yet

--- src/index.js
+++ src/index.js
@@ -12,4 +12,40 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys").then((response) => {
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+    data.forEach(element => {
+      let myCard = document.createElement("div");
+      myCard.classList.add("card");
+
+      let myName = document.createElement("h2");
+      myName.textContent = element.name;
+      myCard.appendChild(myName);
+
+
+      let myImg = document.createElement("img");
+      myImg.classList.add("toy-avatar");
+      myImg.src = element.image;
+      myCard.appendChild(myImg);
+
+
+      let myLikes = document.createElement("p");
+      myLikes.textContent = `Likes: ${element.likes}`;
+      myCard.appendChild(myLikes);
+
+      let likeBtn = document.createElement("button");
+      likeBtn.classList.add("like-btn");
+      likeBtn.textContent = "Like ❤️";
+      myCard.appendChild(likeBtn);
+
+      likeBtn.addEventListener("click", () => {
+        myLikes.textContent = `Likes: ${element.likes += 1}`;
+      });
+
+      document.querySelector("#toy-collection").appendChild(myCard);
+    });
+  })
 });
